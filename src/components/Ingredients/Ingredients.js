1695b9943cdff59572486703db216cd806d37443
nigmasilmi <sn.silmi@gmail.com>
2022-04-09 20:15:34 -0400

    useState, useEffect, useCallback

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,16 +1,95 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
 import IngredientForm from './IngredientForm';
+import IngredientList from './IngredientList';
 import Search from './Search';
 
-function Ingredients() {
+function Ingredients(props) {
+  const [ingredients, setIngredients] = useState([]);
+
+  // const addIngredientHandler = (ingred) => {
+  //   setIngredients((prevIngredients) => [
+  //     ...prevIngredients,
+  //     {
+  //       id: Math.random().toString(),
+  //       title: ingred.title,
+  //       amount: ingred.amount,
+  //     },
+  //   ]);
+  // };
+
+  // useEffect(() => {
+  //   fetchIngredientsFromDB();
+  // }, []);
+
+  // const fetchIngredientsFromDB = () => {
+  //   fetch(process.env.REACT_APP_FIREBASE_ENDPOINT).then((response) => {
+  //     return response
+  //       .json()
+  //       .then((data) => {
+  //         const loadedIngredients = [];
+  //         for (let key in data) {
+  //           loadedIngredients.push({
+  //             id: key,
+  //             title: data[key].title,
+  //             amount: data[key].amount,
+  //           });
+  //         }
+  //         setIngredients(loadedIngredients);
+  //       })
+  //       .catch((err) => {
+  //         console.log(err);
+  //       });
+  //   });
+  // };
+
+  const saveIngredientInDB = (ingredient) => {
+    console.log(JSON.stringify(ingredient));
+    fetch(process.env.REACT_APP_FIREBASE_ENDPOINT, {
+      method: 'POST',
+      body: JSON.stringify(ingredient),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((response) => {
+        // .json() extracts the body and converts it to a javascript object
+        // returns a promise
+        return response.json();
+      })
+      .then((data) => {
+        setIngredients((prevIngredients) => [
+          ...prevIngredients,
+          { id: data.name, ...ingredient },
+        ]);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const removeItemHandler = (ingredientId) => {
+    const newIngredientsList = ingredients.filter(
+      (ele) => ele.id !== ingredientId
+    );
+    setIngredients(newIngredientsList);
+  };
+
+  const queryedIngredientsHandler = useCallback((comingIngredients) => {
+    //
+    setIngredients(comingIngredients);
+  }, []);
+
   return (
     <div className="App">
-      <IngredientForm />
+      <IngredientForm onAddIngredient={saveIngredientInDB} />
 
       <section>
-        <Search />
-        {/* Need to add list here! */}
+        <Search onFilteredIngredients={queryedIngredientsHandler} />
+        <IngredientList
+          onRemoveItem={removeItemHandler}
+          ingredients={ingredients}
+        />
       </section>
     </div>
   );
--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,15 +1,49 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Card from '../UI/Card';
 import './Search.css';
 
-const Search = React.memo(props => {
+const Search = React.memo(({ onFilteredIngredients }) => {
+  // filter by state
+  const [filterBy, setFilterBy] = useState('');
+
+  useEffect(() => {
+    const query =
+      filterBy.length === 0 ? '' : `?orderBy="title"&equalTo="${filterBy}"`;
+    // perform the search
+    fetch(`${process.env.REACT_APP_FIREBASE_ENDPOINT}${query}`).then(
+      (response) => {
+        return response
+          .json()
+          .then((data) => {
+            const loadedIngredients = [];
+            for (let key in data) {
+              loadedIngredients.push({
+                id: key,
+                title: data[key].title,
+                amount: data[key].amount,
+              });
+            }
+            // perform
+            onFilteredIngredients(loadedIngredients);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      }
+    );
+  }, [filterBy, onFilteredIngredients]);
+
   return (
     <section className="search">
       <Card>
         <div className="search-input">
           <label>Filter by Title</label>
-          <input type="text" />
+          <input
+            type="text"
+            value={filterBy}
+            onChange={(event) => setFilterBy(event.target.value)}
+          />
         </div>
       </Card>
     </section>
